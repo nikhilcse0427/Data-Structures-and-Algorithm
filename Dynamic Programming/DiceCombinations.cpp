4b2e8ad2c0fd4a0e444227122c8595e48701c8b7
Nikhil <nikhilverma0427@gmail.com>
2025-03-21 09:53:28 +0530

    Dice combinations from cses problem set solved

---
+++ Dynamic Programming/DiceCombinations.cpp
@@ -0,0 +1,31 @@
+/**
+ x->target sum reqired
+ all possible ways we cax get x by throwixg dices
+ */
+
+ #include<iostream>
+ #include<vector>
+ #define mod 1000000007
+ #define ll long long int 
+ using namespace std;
+ 
+ vector<ll> dp(1000005, -1);
+ ll f(int x) {
+     if (x == 0) return 1;
+     if (dp[x] != -1) return dp[x];
+     
+     ll result = 0; 
+     for (int i = 1; i <= 6; i++) {
+         if(x-i < 0) break;
+         result = (result % mod + f(x - i) % mod) % mod;
+     }
+     
+     return dp[x] = result % mod;
+ }
+ 
+ int main() {
+     int x;
+     cin >> x;
+     cout << f(x);
+     return 0;
+ }
