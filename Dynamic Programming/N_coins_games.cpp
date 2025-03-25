f9822c3ebd649992cffdd6c0aace5f1561d076cc
Nikhil <nikhilverma0427@gmail.com>
2025-03-26 00:01:23 +0530

    feat: add initial implementation for N coins games in N_coins_games.cpp

---
+++ Dynamic Programming/N_coins_games.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+  int n, k, l;
+  cin>>n>>k>>l;
+  vector<int>coins(n);
+  cout<<"enter coins of n size: ";
+  for(int i=0;i<n;i++){
+    cin>>coins[i];
+  }
+}
